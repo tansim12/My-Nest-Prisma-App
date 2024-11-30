3ccacbd52ea9401687235cfc69beb83a0e9bd13f
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-30 15:44:11 +0600

    class provider using different type

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { ArtistModule } from './artist/artist.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SongsModule, ArtistModule],
+  imports: [SongsModule, ArtistModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
