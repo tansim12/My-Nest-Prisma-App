cad4bcd91febe2e0086f1d4ce35700467287f089
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-11-30 18:36:18 +0600

    shared module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserProfileModule } from 'src/user-profile/user-profile.module';
 
 @Module({
+  imports: [UserProfileModule],
   controllers: [UserController],
   providers: [{ provide: 'UserService', useClass: UserService }],
+  exports: [UserProfileModule],
 })
 export class UserModule {}
